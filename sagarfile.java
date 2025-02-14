188e80d672f971e20044e9fe285ceb6e7491f213
sagar <sagar_csd@srkrec.edu.in>
2025-02-14 15:30:48 +0530

    pull req

--- sagarfile.java
+++ sagarfile.java
@@ -2,6 +2,6 @@ public class sagarfile {
   public static void main(String[] args) {
     System.out.println("hello");
     System.out.println("hi");
-    System.out.println("first pull request ");
+    System.out.println("first pull request");
   }
 }
